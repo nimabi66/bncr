22c9f5e5d19fb655fe5acb66725871dfeb586bff
nimabi66 <77653967+nimabi66@users.noreply.github.com>
2024-11-23 16:47:02 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
-# 限制 node /bncr/start.js 运行 10 秒后强制停止
-timeout 60s node /bncr/start.js
+# 启动 Node.js 脚本
+node /bncr/start.js &
+NODE_PID=$!
+
+# 等待 10 秒后发送 SIGTERM 信号
+sleep 70
+kill -SIGTERM $NODE_PID
